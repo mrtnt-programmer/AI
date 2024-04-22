19db22cc0c60ce2b6acd8afcb4eb43786b24ed96
Steph <stephalamy@gmail.com>
2024-04-22 20:31:47 +0200

    test calibrage

--- TakeITEasy/test_calibrage_positions.py
+++ TakeITEasy/test_calibrage_positions.py
@@ -1,33 +1,34 @@
 from functions import *
 
-for name in ['/hex5', '/perf']:
+# central piece:
+#120 120
+# coors = [[None    ,None     ,[340,70] ,[455,135],[570,200]],
+#         [ None    ,[220,130],[340,200],[455,265],[570,335]],
+#         [[105,195],[220,260],[340,340],[455,405],[570,465]],
+#         [[105,335],[220,400],[340,470],[455,535],None     ],
+#         [[105,465],[220,530],[340,600],None     ,None     ]]
+#
+# size = [30,30]
+
+def coor_calc(i,j):
+    if (i,j) in [(0,0),(0,1),(1,0),(3,4),(4,3),(4,4)]:
+        return None
+    shift = 60
+    return 402 + int((j-2)*2*shift*0.98), 129 + int((i*2+j-2) * shift *1.13)
+
+for name in ['/perf', '/hex1', '/hex2', '/hex3', '/hex4', '/hex5']:
     img = cv.imread(path + name + '.jpg')
     print(path + name + '.jpg')
     img = normalize_image(img)
-    affiche(img)
     
     temp = img.copy()
-    i = 0
-    for lines in coors:
-      j=0
-      for coor in lines:
+
+    for i in range(5):
+      for j in range(5):
+        coor = coor_calc(i,j)
         if coor is not None:
-    			#print(coor)
-    			#cv.rectangle(temp,coor, np.sum([coor,size],axis=0), (0,0,255), thickness=2)
-          part = [90,90]
-          part = np.divide((part),(100,100))
-          rect0 = [int(coor[0]+(part[0]*0)),int(coor[1]+(part[1]*0)),int(coor[0]+(part[0]*100)+size[0]),int(coor[1]+part[1]*100+size[1])]
-          rect1 = [int(coor[0]+(part[0]*48)),int(coor[1]+(part[1]*13)),int(coor[0]+(part[0]*52)+size[0]),int(coor[1]+part[1]*17+size[1])]
-          rect2 = [int(coor[0]+(part[0]*10)),int(coor[1]+(part[1]*80)),int(coor[0]+(part[0]*15)+size[0]),int(coor[1]+part[1]*90+size[1])]
-          rect3 = [int(coor[0]+(part[0]*85)),int(coor[1]+(part[1]*80)),int(coor[0]+(part[0]*90)+size[0]),int(coor[1]+part[1]*90+size[1])]
-          
-          cv.rectangle(temp,(rect0[0],rect0[1]),(rect0[2],rect0[3]),(0,255,255), thickness=2)#top
-          cv.rectangle(temp,(rect1[0],rect1[1]),(rect1[2],rect1[3]),(255,255,0), thickness=2)#top
-          cv.rectangle(temp,(rect2[0],rect2[1]),(rect2[2],rect2[3]), (255,0,0), thickness=2)#left
-          cv.rectangle(temp,(rect3[0],rect3[1]),(rect3[2],rect3[3]), (255,0,0), thickness=2)#right
-          coors[i][j] = [rect1,rect2,rect3]
-        j += 1
-      i += 1
-    affiche(temp)
+          cv.circle(temp,coor,8,(0,0,0),-1)
+
+    affiche(temp,name)
 
 
