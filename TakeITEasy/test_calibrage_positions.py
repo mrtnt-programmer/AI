71493bb0094f5cc2bf548ae44e98685c3e9ae033
Steph <stephalamy@gmail.com>
2024-04-24 19:41:47 +0200

    calibrage ok

--- TakeITEasy/test_calibrage_positions.py
+++ TakeITEasy/test_calibrage_positions.py
@@ -1,14 +1,11 @@
 from functions import *
+from math import sqrt
 
-# central piece:
-#120 120
-# coors = [[None    ,None     ,[340,70] ,[455,135],[570,200]],
-#         [ None    ,[220,130],[340,200],[455,265],[570,335]],
-#         [[105,195],[220,260],[340,340],[455,405],[570,465]],
-#         [[105,335],[220,400],[340,470],[455,535],None     ],
-#         [[105,465],[220,530],[340,600],None     ,None     ]]
-#
-# size = [30,30]
+def draw_rect(img, coor, w, h=-1, color=(255,255,255)):
+    h = w if h == -1 else h
+    top_left = (coor[0] - w//2, coor[1] - h//2)
+    bottom_right = (coor[0] + w//2, coor[1] + h//2)
+    cv.rectangle(img,top_left,bottom_right,color,thickness=2)
 
 def coor_calc(i,j):
     if (i,j) in [(0,0),(0,1),(1,0),(3,4),(4,3),(4,4)]:
@@ -16,19 +13,32 @@ def coor_calc(i,j):
     shift = 60
     return 402 + int((j-2)*2*shift*0.98), 129 + int((i*2+j-2) * shift *1.13)
 
+def coor_number(coor, name):
+  radius = 44
+  if name == 'top':
+    return coor[0], coor[1]-radius
+  if name == 'left':
+    return int(coor[0] - radius*sqrt(3)/2), int(coor[1]+radius/2)
+  if name == 'right':
+    return int(coor[0] + radius*sqrt(3)/2), int(coor[1]+radius/2)
+
 for name in ['/perf', '/hex1', '/hex2', '/hex3', '/hex4', '/hex5']:
-    img = cv.imread(path + name + '.jpg')
-    print(path + name + '.jpg')
-    img = normalize_image(img)
-    
-    temp = img.copy()
+# for name in ['/hex1']:
+  img = cv.imread(path + name + '.jpg')
+  print(path + name + '.jpg')
+  img = normalize_image(img)
 
-    for i in range(5):
-      for j in range(5):
-        coor = coor_calc(i,j)
-        if coor is not None:
-          cv.circle(temp,coor,8,(0,0,0),-1)
+  temp = img.copy()
 
-    affiche(temp,name)
+  for i in range(5):
+    for j in range(5):
+      coor = coor_calc(i,j)
+      if coor is not None:
+        cv.circle(temp,coor,8,(0,0,0),-1)
+        for num in ['top','left','right']:
+          coor_num = coor_number(coor, num)
+          draw_rect(temp,coor_num,55)
+
+  affiche(temp,name)
 
 
